f0ff8e36e45561df66d35f50e7b524a39a30c902
Elias Holzer <elias@vvvv.org>
2025-05-27 18:33:52 +0200

    first build
    - download and copy premake5.exe to native
    - start vs developer command prompt
    - make sure https://github.com/skeeto/w64devkit/releases is in PATH
    - `premake5 vs2022`

--- native/premake5.lua
+++ native/premake5.lua
@@ -1,32 +1,59 @@
-workspace "vl-rive-runtime"
-configurations { "Debug", "Release" }
-platforms { "x64", "ARM64" }
+RIVE_RUNTIME_DIR = path.getabsolute("../submodules/rive-runtime")
+premake.path = premake.path .. ";" .. RIVE_RUNTIME_DIR .. "/build"
 
-RIVE_RUNTIME_DIR = "../submodules/rive-runtime"
+dofile(RIVE_RUNTIME_DIR .. "/premake5_v2.lua")
+dofile(RIVE_RUNTIME_DIR .. "/renderer/premake5_pls_renderer.lua")
+dofile(RIVE_RUNTIME_DIR .. '/decoders/premake5_v2.lua')
 
-project("rive")
+--workspace "vl-rive-runtime"
+--configurations { "Debug", "Release" }
+--platforms { "x64", "ARM64" }
+
+
+project("vl-rive-interop")
+dependson("rive")
+dependson("rive-decoders")
+dependson("rive-renderer-pls")
 kind("SharedLib")
 language("C++")
 cppdialect("C++17")
+
+links({
+    'rive',
+    'rive_pls_renderer',
+    'rive_decoders',
+    'libwebp',
+    'rive_harfbuzz',
+    'rive_sheenbidi',
+    'rive_yoga',
+})
+
+links({ 'zlib', 'libpng' })
+links({ 'libjpeg' })
+
+links({ 'd3dcompiler' })
+--links({ 'd3d11', 'dxguid', 'dxgi', 'd3dcompiler' })
+
 -- Remove the generic targetdir, set per-platform below
 objdir("obj/%{cfg.platform}/%{cfg.buildcfg}")
-staticruntime("off") -- /MD for dll
+--staticruntime("off") -- /MD for dll
 toolset("clang")
 
 includedirs({ RIVE_RUNTIME_DIR .. "/include", RIVE_RUNTIME_DIR .. "/renderer/include" })
-files({ RIVE_RUNTIME_DIR .. "/src/**.cpp", "RiveSharpInterop.cpp" })
+--files({ RIVE_RUNTIME_DIR .. "/src/**.cpp", RIVE_RUNTIME_DIR .. "/renderer/src/*.cpp", RIVE_RUNTIME_DIR .. "/renderer/src/d3d11/*.cpp", RIVE_RUNTIME_DIR .. "/renderer/src/d3d/*.cpp", "RiveSharpInterop.cpp" })
+files({ "RiveSharpInterop.cpp" })
 
 -- this is building the actual rive library so it seems we need this here.
-defines({ "_RIVE_INTERNAL_" })
+--defines({ "_RIVE_INTERNAL_" })
 
-filter("configurations:Debug")
-defines({ "DEBUG" })
-symbols("On")
+--filter("configurations:Debug")
+--defines({ "DEBUG" })
+--symbols("On")
 
-filter("configurations:Release")
-defines({ "RELEASE" })
-defines({ "NDEBUG" })
-optimize("Size")
+--filter("configurations:Release")
+--defines({ "RELEASE" })
+--defines({ "NDEBUG" })
+--optimize("Size")
 
 filter("platforms:x64")
   architecture("x64")
---
+++ src/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VL.Rive
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var x = CreateRenderContextD3D11(IntPtr.Zero, IntPtr.Zero, IntPtr.Zero);
+
+
+            // This is a placeholder for the main entry point of the application.
+            // You can add your application logic here.
+            Console.WriteLine("Welcome to VL.Rive!");
+            Console.ReadLine(); // Wait for user input before closing
+
+        }
+
+        [DllImport("vl-rive-interop.dll")]
+        static extern IntPtr CreateRenderContextD3D11(IntPtr a, IntPtr b, IntPtr c);
+    }
+}
